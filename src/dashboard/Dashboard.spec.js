bcca0aa96c02aaf3f8b01aa1ad659e05dfc2e6cc
ntsum <natsukajiwaram@gmail.com>
2019-05-15 16:30:50 -0400

    dashboard test passing and complete

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
 // Test away
+import React from "react";
+import { render } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import Dashboard from "./Dashboard";
+
+describe("Dashboard component tests", () => {
+  test("the dashboard renders the control and display", () => {
+    const { getByText } = render(<Dashboard />);
+
+    getByText(/unlocked/i);
+    getByText(/open/i);
+    getByText(/lock gate/i);
+    getByText(/close gate/i);
+  });
+});
